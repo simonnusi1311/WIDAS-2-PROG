a79780f793b59c34dde0a8fdbc82e89493f9215e
Simon Nuspahic <simon.nuspahic@gmail.com>
2025-06-14 22:24:06 +0200

    Kleine Abänderungen von der Description

--- Abgabe_12/src/thd/screens/GameInfo.java
+++ Abgabe_12/src/thd/screens/GameInfo.java
@@ -13,8 +13,9 @@ public class GameInfo {
     /**
      * Game Description.
      */
-    public static final String DESCRIPTION = "Steuern Sie den JetFighter bis Level 10, um das Spiel zu gewinnen.\n"
-            + "Bewegen Sie sich horizontal mit den Taste A/D oder den Pfeiltasten, beschleunigen Sie mit W und feuern Sie mit der Leertaste.\n"
+    public static final String DESCRIPTION = "Steuern Sie den JetFighter bis Level 12, um das Spiel zu gewinnen.\n\n"
+            + "Bewegen Sie sich horizontal mit den Taste A/D oder den Pfeiltasten.\n"
+            + "Beschleunigen Sie mit W (oder Pfeiltaste) und feuern Sie mit der Leertaste.\n\n"
             + "Vermeiden Sie Kollisionen mit Gegnern und den Rändern.\n"
             + "Tipp: Achten Sie auf die Panzer am Rand und beschleunigen Sie, wenn sie schießen!";
 }
